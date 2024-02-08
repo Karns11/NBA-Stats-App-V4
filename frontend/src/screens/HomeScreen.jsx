18f42df70badb6c07720f02bccd8376f3a063b12
nathan karns <Nathan k>
2024-02-08 18:36:17 -0500

    fix home screen accessibility

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "../components/Hero";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const HomeScreen = () => {
+  const navigate = useNavigate();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate("/NBA-App-Home");
+    }
+  }, [navigate, userInfo]);
+
   return (
     <div>
       <Hero />
